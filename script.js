50829065f1a1c1505133fb6d87af40e33d3a7bf7
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-12-03 11:35:58 +0100

    js

--- script.js
+++ script.js
@@ -10,5 +10,31 @@ const displayMessage = (message) => {
 };
 
 document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
 
+    if (!guess) {
+        displayMessage('No number!');
+    } else if (guess === secretNum) {
+        displayMessage('Correct number!');
+        document.querySelector('.number').textContent = secretNum;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '40rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    } else if (guess !== secretNum) {
+        if (score > 1) {
+            displayMessage(guess > secretNum ? 'TOO HIGH!' : 'too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "You Lost :(";
+            document.querySelector('.score').textContent = 0;
+        }
+    } else {
+        document.querySelector('.message').textContent = 'You Lost :(';
+        document.querySelector('.score').textContent = 0;
+    }
 });
